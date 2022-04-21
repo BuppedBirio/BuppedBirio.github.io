fb2ff0f69c4d7def537f7826e0f6c3d112dbedc2
Birio <lalucenelloscurita@gmail.com>
2022-04-21 22:17:49 +0200

    local storage and some fix

--- language.js
+++ language.js
@@ -1,19 +1,30 @@
-$('[lang]').hide();
-$('[lang="en"]').show();
-
-$('#lang-switch').change(function () {
-    var lang = $(this).val();
+function setLanguage(lang) {
     switch (lang) {
     case 'en':
         $('[lang]').hide();
         $('[lang="en"]').show();
+        
+        localStorage.setItem("language", "en");
+        document.getElementById("lang-switch").options[0].selected = true;
     break;
     case 'it':
         $('[lang]').hide();
         $('[lang="it"]').show();
+
+        localStorage.setItem("language", "it");
+        document.getElementById("lang-switch").options[1].selected = true;
     break;
     default:
         $('[lang]').hide();
         $('[lang="en"]').show();
+
+        localStorage.setItem("language", "en");
+        document.getElementById("lang-switch").options[0].selected = true;
     }
+}
+
+setLanguage(localStorage.getItem("language"));
+
+$('#lang-switch').change(function () {
+    setLanguage($(this).val());
 });
